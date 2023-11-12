c1ac249aa5c4b94d3eed023704e1db4c41edf29d
feverdreme <jeffreyhuang08@gmail.com>
2023-11-12 04:05:55 -0500

    implemented /students/register

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -2,23 +2,21 @@ import * as functions from 'firebase-functions'
 import admin from 'firebase-admin';
 import express from 'express';
 import cors from 'cors';
+
 import students from './students';
+
 //import bodyParser from 'body-parser';
 const serviceAccount: string = require('../service-account.json')
 
 // initialize firebase inorder to access its services
-admin.initializeApp({credential: admin.credential.cert(serviceAccount)});
+const firebaseApp = admin.initializeApp({credential: admin.credential.cert(serviceAccount)});
 
 const app = express();
-const students = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/students", students)
 
-students.get('/', (req, res) => {
-  res.send('hello!');
-});
-
-
 exports.api = functions.https.onRequest(app)
+
+export {firebaseApp};
--- backend/functions/src/students.ts
+++ backend/functions/src/students.ts
@@ -1,14 +1,48 @@
 import express from 'express';
+import { firebaseApp } from './index';
 
 const students = express();
+
+interface student {
+  firstName: string;
+  lastName: string;
+  gradYear: number;
+  misc?: object;
+}
+
+function isStudent(data: object): data is student {
+  return 'firstName' in data && 'lastName' in data && 'gradYear' in data;
+}
+
 // TODO: use express-validator middleware
 
-students.post('/register', async (req, res) => {});
+/**
+ * Creates a new student in the `registrants` collection using x-www-form-urlencoded data.
+ * 
+ * Upon success returns the document ID with HTTP 202
+ * Upon fail returns HTTP 400
+ */
+students.post('/register', async (req, res) => {
+  let studentData = req.body;
 
-students.put("/updateStudent", async (req, res) => {});
+  if (!isStudent(studentData)) {
+    res.status(400).send('Malformed student registration request.');
+    return;
+  }
 
-students.get("/query", async (req, res) => {});
+  // When a student is registered it should send the document ID of the newly created student
+  let ref = await firebaseApp
+    .firestore()
+    .collection('registrants')
+    .add(studentData);
 
-students.get("/queryAll", async (req, res) => {});
+  res.status(202).send(ref.id);
+});
+
+students.put('/updateStudent', async (req, res) => {});
+
+students.get('/query', async (req, res) => {});
+
+students.get('/queryAll', async (req, res) => {});
 
 export default students;

2afbe531f8f0d08ef034eadabbb66feb32651fb2
CollinWoo <collinwu01@gmail.com>
2023-11-29 15:36:54 -0500

    export firestore database reference

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,16 +1,20 @@
-import * as functions from 'firebase-functions'
+import * as functions from 'firebase-functions';
 import admin from 'firebase-admin';
 import express from 'express';
 import cors from 'cors';
 //import bodyParser from 'body-parser';
-const serviceAccount: string = require('../service-account.json')
+const serviceAccount: string = require('../service-account.json');
 
 // initialize firebase inorder to access its services
-admin.initializeApp({credential: admin.credential.cert(serviceAccount)});
+admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 
 //initialize express server
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-exports.api = functions.https.onRequest(app)
+const db = admin.firestore();
+
+exports.api = functions.https.onRequest(app);
+
+export { db };

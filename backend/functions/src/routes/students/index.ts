e6f76d4da44a85a517b1e1dc9c57761f920ef721
feverdreme <jeffreyhuang08@gmail.com>
2024-01-16 18:36:49 -0500

    Minor formatting changes

--- backend/functions/src/routes/students/index.ts
+++ backend/functions/src/routes/students/index.ts
@@ -3,6 +3,7 @@ import { db } from '../../index';
 
 const students = express.Router();
 
+// TODO: Convert to middleware
 function catchAll(
   func: (req: Request, res: Response) => Promise<void>
 ): (req: Request, res: Response) => Promise<void> {
@@ -22,131 +23,128 @@ function catchAll(
  * Upon fail returns HTTP 400
  */
 students.post('/', catchAll(async (req, res) => {
-    let studentData = req.body;
+  let studentData = req.body;
 
-    // Validate form data
-    if (!isStudent(studentData)) {
+  // Validate form data
+  if (!isStudent(studentData)) {
+    res.status(400).send({
+      error:
+        'Malformed student registration request. Missing some required fields.',
+    });
+    return;
+  }
+
+  // Check email not in database already
+  try {
+    let docRef = await db.collection('students').doc(studentData.email).get();
+
+    if (docRef.exists) {
       res.status(400).send({
-        error:
-          'Malformed student registration request. Missing some required fields.',
+        error: `Student with email: ${studentData.email} already exists.`,
       });
       return;
     }
+  } catch (e) {
+    res.status(500).send({ error: `Problem with email validation.` });
+  }
 
-    // Check email not in database already
-    try {
-      let docRef = await db.collection('students').doc(studentData.email).get();
-      if (docRef.exists) {
-        res.status(400).send({
-          error: `Student with email: ${studentData.email} already exists.`,
-        });
-        return;
-      }
-    } catch (e) {
-      res.status(500).send({ error: `Problem with email validation.` });
-    }
+  // Create student with email
+  try {
+    await db.collection('students').doc(studentData.email).set(studentData);
+  } catch (e) {
+    res.status(400).send({ error: 'Could not create student.' });
+    return;
+  }
 
-    try {
-      await db.collection('students').doc(studentData.email).set(studentData);
-    } catch (e) {
-      res.status(400).send({ error: 'Could not create student.' });
-      return;
-    }
-
-    res.sendStatus(200);
-  })
-);
+  res.sendStatus(200);
+}));
 
 /**
  * Updates a student with new parameters. Does not support updating arrays yet.
  */
 students.put('/:email', catchAll(async (req, res) => {
-    let email = req.params.email;
-    let studentData = req.body;
+  let email = req.params.email;
+  let studentData = req.body;
 
-    // Check that email field exists on req.body
-    if (!isStudentMutation(studentData)) {
-      res.status(404).send({
-        error: 'Student email not specified.',
-      });
-      return;
-    }
+  // Check that email field exists on req.body
+  if (!isStudentMutation(studentData)) {
+    res.status(404).send({
+      error: 'Student email not specified.',
+    });
+    return;
+  }
 
-    try {
-      await db
-        .collection('students')
-        .doc(email)
-        // @ts-ignore
-        .update(studentData);
-    } catch (e) {
-      res.status(404).send({
-        error: `Student with email: ${email} could not be updated.`,
-      });
-      return;
-    }
+  try {
+    await db
+      .collection('students')
+      .doc(email)
+      // @ts-ignore
+      .update(studentData);
+  } catch (e) {
+    res.status(404).send({
+      error: `Student with email: ${email} could not be updated.`,
+    });
+    return;
+  }
 
-    res.sendStatus(200);
-  })
-);
+  res.sendStatus(200);
+}));
 
 /**
  * Retrives all students.
  */
 students.get('/', catchAll(async (req, res) => {
-    let collectionRef = db.collection('students');
-    let snapshotRef = await collectionRef.get();
+  let collectionRef = db.collection('students');
+  let snapshotRef = await collectionRef.get();
 
-    let addedData: Map<string, student> = new Map();
-    snapshotRef.forEach((doc) => {
-      addedData.set(doc.id, doc.data() as student);
-    });
+  let addedData: Map<string, student> = new Map();
+  snapshotRef.forEach((doc) => {
+    addedData.set(doc.id, doc.data() as student);
+  });
 
-    res.status(200).send(Object.fromEntries(addedData));
-  })
-);
+  res.status(200).send(Object.fromEntries(addedData));
+}));
 
 /**
  * Retrieves student with specificed `email` param.
  * Eg. GET /students/jrh382@cornell.edu
  */
 students.get('/:email', catchAll(async (req, res) => {
-    let email = req.params.email;
+  let email = req.params.email;
 
-    let collectionRef = db.collection('students');
-    let docRef = await collectionRef.doc(email).get();
+  let collectionRef = db.collection('students');
+  let docRef = await collectionRef.doc(email).get();
 
-    if (docRef.exists) {
-      res.status(200).send(docRef.data());
-    } else {
-      res
-        .status(404)
-        .send({ error: `Could not get data from email: ${email}` });
-    }
-  })
-);
+  if (docRef.exists) {
+    res.status(200).send({[email]: docRef.data()});
+  } else {
+    res
+      .status(404)
+      .send({ error: `Could not get data from email: ${email}` });
+  }
+}));
 
 /**
  * Deletes student from the students collection.
  */
 students.delete('/:email', catchAll(async (req, res) => {
-    let email = req.params.email;
-    let collectionRef = db.collection('students');
+  let email = req.params.email;
+  let collectionRef = db.collection('students');
 
-    // Handle when email not specified
-    if (email == '') {
-      res.status(400).send({ error: 'Email must be specified.' });
-      return;
-    }
+  // Handle when email not specified
+  if (email == '') {
+    res.status(400).send({ error: 'Email must be specified.' });
+    return;
+  }
 
-    try {
-      await collectionRef.doc(email).delete();
-    } catch (e) {
-      res.status(404).send({ error: `Cannot find email: ${email}` });
-      return;
-    }
+  try {
+    await collectionRef.doc(email).delete();
+  } catch (e) {
+    res.status(404).send({ error: `Cannot find email: ${email}` });
+    return;
+  }
 
-    res.status(200).send(`Successfully deleted: ${email}`);
-  })
-);
+  res.status(200).send(`Successfully deleted: ${email}`);
+}));
 
 export default students;

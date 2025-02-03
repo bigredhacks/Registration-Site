ac937d068ac0fd21a651551578c009e47d0b60d2
Colin Wu <45516888+CollinWoo@users.noreply.github.com>
2025-02-03 17:33:37 -0500

    fix .env file handling

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017";
 
-export const connectDB = async () => {
+
+export const connectDB = async (uri: string) => {
+
   try {
-    await mongoose.connect(MONGO_URI);
-    console.log(`✅ MongoDB connected: ${MONGO_URI}`);
+    await mongoose.connect(uri);
+    console.log(`✅ MongoDB connected: ${uri}`);
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
     process.exit(1); // Exit process on failure
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,18 @@ import cors from 'cors';
 import { connectDB } from './config/db';
 import dotenv from "dotenv";
 
-dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
-connectDB();
+const NODE_ENV = process.env.NODE_ENV || "development";
+
+dotenv.config({ path: `.env.${NODE_ENV}` });
+
+const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017";
+const PORT = process.env.PORT || 5000;
+
+connectDB(MONGO_URI);
 
 //initialize express server
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 
 app.use(express.json());
 app.use(cors());

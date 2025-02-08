2fcabf3933c11bf3fbb9137b73581958f7a783e5
James Tu <jamestvvo@gmail.com>
2025-02-08 03:13:35 -0500

    update db.ts to take in mongo connection string

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
 
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017";
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,8 +13,11 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware
 app.use(express.json());
 app.use(cors());
+
+// Routes
 app.use('/layouts', layoutsRouter);
 
 app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));

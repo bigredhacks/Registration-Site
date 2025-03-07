739f942abb5c4134dd59c417c092fadff66d1b46
Liesel <lyw8@cornell.edu>
2025-03-06 23:19:40 -0500

    update eslintrc file to remove next/babelrc error and fix error on LoginLink page

--- frontend/src/app/mui-components/LoginLink.tsx
+++ frontend/src/app/mui-components/LoginLink.tsx
@@ -4,17 +4,15 @@ import { styled } from '@mui/system';
 interface CustomComponentProps {
   children: React.ReactNode;
   href: string;
-  sx?: SxProps<Theme>; // Add sx prop to handle custom styles
+  sx?: SxProps; // Add sx prop to handle custom styles
 }
 
-// Define styled component without directly setting href
-const LoginLinks = styled(Link)(({ theme }) => ({
+const LoginLinksStyling = styled(Link)(({ theme }) => ({
   display: "block", 
   marginTop: theme.spacing(1), 
   color: "#a55c4a",
 }));
 
-// The component where you define the href and accept sx
-export default function CustomComponent({ children, href, sx }: CustomComponentProps) {
-  return <LoginLinks href={href} sx={sx}>{children}</LoginLinks>;
+export default function LoginLinks({ children, href, sx }: CustomComponentProps) {
+  return <LoginLinksStyling href={href} sx={sx}> {children}</LoginLinksStyling>;
 }
